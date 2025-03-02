6783e23a7b05f141cf2e36942815dcb98653e734
Vikash Mall <vikashmall4242@gmail.com>
2025-03-02 16:58:46 -0600

    p1

---
+++ Unit 1/Session 1/Standard/Version 1/p1.py
@@ -0,0 +1,18 @@
+# Problem 1: Hundred Acre Wood
+# Write a function welcome() that prints the string "Welcome to The Hundred Acre Wood!".
+
+# def welcome():
+# 	pass
+# Example Usage:
+
+# welcome()
+# Example Output:
+
+# Welcome to The Hundred Acre Wood!
+
+
+
+def welcome():
+	print("Welcome to The Hundred Acre Wood!")
+
+welcome()
