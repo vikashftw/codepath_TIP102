6119245058a5f57f60e56e5af59cfa1ff5914a8b
Vikash Mall <vikashmall4242@gmail.com>
2025-03-02 17:00:03 -0600

    p1

---
+++ Unit 1/Session 1/Standard/Version 2/p1.py
@@ -0,0 +1,18 @@
+# Problem 1: Batman
+# Write a function batman() that prints the string "I am vengeance. I am the night. I am Batman!".
+
+# def batman():
+# 	pass
+# Example Usage:
+
+# batman()
+# Example Output:
+
+# I am vengeance. I am the night. I am Batman!
+
+
+
+def batman():
+	print("I am vengeance. I am the night. I am Batman!")
+
+batman()
